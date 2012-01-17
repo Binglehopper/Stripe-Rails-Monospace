6b8dfabe4b435f43c7c91e603532a99c6fe83399
Gregory Lou <gregory.lou@gmail.com>
2012-01-16 22:23:02 -0500

    added private token

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,4 @@
-Stripe.api_key = "QiBCmFOy0z8zlKCK5wCIj8vKKrNsvmEM"
-STRIPE_PUBLISHABLE_KEY = "pk_dl4LcpeAxUEPHN3FxzuAQQmhCGmx5"
+# Stripe.api_key = "QiBCmFOy0z8zlKCK5wCIj8vKKrNsvmEM"
+# STRIPE_PUBLISHABLE_KEY = "pk_dl4LcpeAxUEPHN3FxzuAQQmhCGmx5"
+Stripe.api_key = "spGBUke0FaBSUi0uVbFm1TYwrAZvLx8f"
+STRIPE_PUBLISHABLE_KEY = "pk_vdevIOQCOwUf3FWVWyzhPQgkvMqpR"
